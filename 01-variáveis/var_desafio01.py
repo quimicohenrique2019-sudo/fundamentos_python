0012bb49f174b20c086ec658c1d705a7edbc7ead
quimicohenrique2019-sudo <quimicohenrique2019@gmail.com>
2026-08-13 21:51:21 -0300

    Variáveis inteiras
    Explicação de Variáveis Inteiras

---
+++ 01-variáveis/var_desafio01.py
@@ -0,0 +1,8 @@
+# Desafio de cálculo de área
+# Área = comprimento * largura
+comprimento = 18.6
+largura = 9.8
+multiplicacao = comprimento * largura
+print('Área:', multiplicacao)
+preço = 125000 / multiplicacao
+print('Valor por metro:', preço)
---
+++ 01-variáveis/var_int.py
@@ -0,0 +1,28 @@
+# Autor: Henrique Xavier Prestes
+# Projeto: Estudando variáveis inteiras
+
+# Nome de variável não pode ter caracteres especiais
+# não pode ser uma palavra reservada
+# (print, if, else, elif, for)
+# não pode ter espaço
+# (valor principal)
+# Nome e tipo:
+valor1 = 12
+valor2 = 22
+soma = valor1 + valor2
+subtracao = valor1 - valor2
+multiplicacao = valor1 * valor2
+divisao = valor1 / valor2
+print(soma)
+print(subtração)
+print(multiplicação)
+print(divisão)
+
+# no python eu posso usar tanto aspas simples quanto duplas
+print('A soma é:  ', soma)
+print("A subtração é:  ", subtracao)
+print('A multiplicação é:  ', multiplicacao)
+print("A divisão é:  ", divisao)
+
+# imprimindo todos os resultados em uma única linha
+print('A soma é:  ', soma, 'e a subtração é:  ', subtracao)
